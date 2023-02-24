d4014a161d9782b8c352630c2425c045cb3848c1
Sabuj Golui <sabujgolui5#gmail.com>
2023-02-24 23:28:43 +0530

    added a rest DB

---
+++ server/db.js
@@ -0,0 +1,11 @@
+// connect to database postgres
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    user: 'postgres',
+    password: 'sabuj',
+    host: 'localhost',
+    port: 5432,
+    database: 'perntodo'
+});
+module.exports = pool;
